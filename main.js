e64706e3b106adfbb70c55f15ede7ca9affd0e38
denis0001-dev <demorozov2048@gmail.com>
2024-10-20 20:54:47 +0300

    Bugfixes

--- main.js
+++ main.js
@@ -281,6 +281,34 @@ document.addEventListener("DOMContentLoaded", async () => {
         videoY = null;
         video.addEventListener("click", scaleVideo);
     });
+    function watchForHover() {
+        var hasHoverClass = false;
+        var container = document.body;
+        var lastTouchTime = 0;
+        function enableHover() {
+            // filter emulated events coming from touch events
+            if (new Date() - lastTouchTime < 500)
+                return;
+            if (hasHoverClass)
+                return;
+            container.className += ' hasHover';
+            hasHoverClass = true;
+        }
+        function disableHover() {
+            if (!hasHoverClass)
+                return;
+            container.className = container.className.replace(' hasHover', '');
+            hasHoverClass = false;
+        }
+        function updateLastTouchTime() {
+            lastTouchTime = new Date();
+        }
+        document.addEventListener('touchstart', updateLastTouchTime, true);
+        document.addEventListener('touchstart', disableHover, true);
+        document.addEventListener('mousemove', enableHover, true);
+        enableHover();
+    }
+    watchForHover();
     // get releases
     const MyOctokit = Octokit.plugin(restEndpointMethods);
     octokit = new MyOctokit({
